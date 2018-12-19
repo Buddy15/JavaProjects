1aff3d03f633fbc934a4ca1950e3f25f733f2b7e
Buddy15 <learningbuddy15@gmail.com>
2018-12-19 10:18:45 +0530

    This is another commit changes

--- GitPractice/src/PrintTesting.java
+++ GitPractice/src/PrintTesting.java
@@ -2,7 +2,7 @@
 public class PrintTesting {
 
 	public static void main(String[] args) {
-		System.out.println("Printing this changes");
+		System.out.println("Printing this changes and lets c");
 	}
 
 }
